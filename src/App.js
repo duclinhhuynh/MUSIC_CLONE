6760b5a7bcf6d3b7f65a27eb16a287ee017b80c2
= <linh1862002@gmail.com>
2023-07-19 23:36:10 +0700

    setup route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import { useSelector, useDispatch } from "react-redux";
+import { Home, Login, Public } from "./containers/public/";
 import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from "react-router-dom";
+import path from "./utils/path";
 function App() {
-  const {homeData} = useSelector(state => state.app)
-  console.log(homeData);
   return (
-  
+    <>
+    <div className="">
+      <Routes>
+        <Route path={path.PUBLIC} element={<Public/>}>
+          <Route path={path.HOME} element={<Home/>} />
+          <Route path={path.LOGIN} element={<Login/>} />
+        </Route>
+      </Routes>
+    </div>
     <ToastContainer
     position="top-right"
     autoClose={5000}
@@ -19,7 +28,7 @@ function App() {
     pauseOnHover
     theme="light"
     />
-     
+  </>     
   );
 }
 
---
+++ src/utils/path.js
@@ -0,0 +1,8 @@
+// save link path in app
+const path = {
+    PUBLIC: '/*',
+    HOME: '',
+    LOGIN: 'login'
+}
+
+export default path
